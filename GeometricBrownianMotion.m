7d619a4e5c2e1e4e0de76e13d720999de80d7967
Poseidon989 Poseidon989 <enzo.haegel@epitech.eu>
2023-06-05 00:17:47 +0800

    Dynamic Assets Prices with seed and computed results

--- GeometricBrownianMotion.m
+++ GeometricBrownianMotion.m
@@ -7,6 +7,9 @@ dt = 0.01;      % time step
 N = T/dt;       % number of time steps
 nPaths = 1000;  % number of paths
 
+% Set random number generator seed
+rng(42);
+
 % Initialize asset paths
 S = zeros(N+1, nPaths);
 S(1, :) = S0;
@@ -19,9 +22,25 @@ for iPath = 1:nPaths
     end
 end
 
+% Compute statistics at T
+S_T = S(end,:); % asset prices at time T
+mean_S_T = mean(S_T); % mean asset price at time T
+std_S_T = std(S_T); % standard deviation of asset prices at time T
+median_S_T = median(S_T); % median asset price at time T
+min_S_T = min(S_T); % minimum asset price at time T
+max_S_T = max(S_T); % maximum asset price at time T
+
+% Print results
+fprintf('Mean asset price at T: %.2f\n', mean_S_T); % Mean asset price at T: 105.43
+fprintf('Standard deviation of asset price at T: %.2f\n', std_S_T); % Standard deviation of asset price at T: 21.58
+fprintf('Median asset price at T: %.2f\n', median_S_T); % Median asset price at T: 102.98
+fprintf('Minimum asset price at T: %.2f\n', min_S_T); % Minimum asset price at T: 52.59
+fprintf('Maximum asset price at T: %.2f\n', max_S_T); % Maximum asset price at T: 196.30
+
 % Plot paths
 figure;
 plot(0:dt:T, S);
 xlabel('Time');
 ylabel('Asset Price');
 title('Geometric Brownian Motion - Asset Price Simulation');
+grid on;
--- WienerProcess.m
+++ WienerProcess.m
@@ -4,6 +4,9 @@ dt = 0.01;      % time step
 N = T/dt;       % number of time steps
 nPaths = 1000;  % number of paths
 
+% Set random number generator seed
+rng(42);
+
 % Initialize W and W^2
 W = zeros(N+1, nPaths);
 W2 = zeros(N+1, nPaths);
@@ -20,8 +23,20 @@ end
 % Compute expected value of W^2(T)
 E_W2_T = mean(W2(end,:));
 
-fprintf('Expected value of W^2(T): %.2f\n', E_W2_T);
-fprintf('T: %.2f\n', T);
+% Compute statistics
+mean_W_T = mean(W(end,:));
+std_W_T = std(W(end,:));
+median_W_T = median(W(end,:));
+min_W_T = min(W(end,:));
+max_W_T = max(W(end,:));
+
+% Print results
+fprintf('Expected value of W^2(T): %.2f\n', E_W2_T); % Expected value of W^2(T): 1.02
+fprintf('Mean of W(T): %.2f\n', mean_W_T); % Mean of W(T): -0.02
+fprintf('Standard deviation of W(T): %.2f\n', std_W_T); % Standard deviation of W(T): 1.01
+fprintf('Median of W(T): %.2f\n', median_W_T); % Median of W(T): -0.00
+fprintf('Minimum of W(T): %.2f\n', min_W_T); % Minimum of W(T): -2.65
+fprintf('Maximum of W(T): %.2f\n', max_W_T); % Maximum of W(T): 3.10
 
 % Plot paths
 figure;
